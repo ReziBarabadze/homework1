8828c048231026438afad6e187332e17f01b7504
Hamasaqalamidi <rezibarabadze153@gmail.com>
2024-09-27 13:36:51 +0400

    add react

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-import React from "React";
+import logo from "./logo.svg";
+import "./App.css";
 
 
-const link = 'https://google.com';
-
-
-
+const link = "https://google.com";
 
 function App() {
   return (
-    <a href={link}>this is my link</a>
-    <input onChange = {}></input>
-    <button onClick = {}></button>
+    <div>
+      <MyButton
+        color="red"
+        text="red button"
+        onClick={() => {
+          console.log("i am red button");
+        }}
+        numbers={[1, 2, 3]}
+      />
+      <MyButton
+        color="yellow"
+        text="yellow button"
+        onClick={() => {
+          alert("i am yellow button");
+        }}
+        numbers={[4, 5, 6]}
+      />
+      <MyButton
+        color="blue"
+        text="blue button"
+        onClick={() => {
+          console.log("i am blue button");
+        }}
+        numbers={[7, 8, 9]}
+      />
+    </div>
   );
 }
+
+const MyButton = ({ color, text, onClick, numbers }) => {
+  console.log(numbers);
+  
+  return (
+    <button
+      style={{
+        width: 100,
+        height: 100,
+        borderRadius: "50%",
+        backgroundColor: color,
+      }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
+};
 
 export default App;
