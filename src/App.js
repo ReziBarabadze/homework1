458dda18ac3cb836cb990a4014ef81f370dbf4a3
Hamasaqalamidi <rezibarabadze153@gmail.com>
2024-09-27 12:34:27 +0400

    add

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "React";
+
+
+const link = 'https://google.com';
+
+
+
+
+function App() {
+  return (
+    <a href={link}>this is my link</a>
+    <input onChange = {}></input>
+    <button onClick = {}></button>
+  );
+}
+
+export default App;
